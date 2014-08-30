4af2a7842baede93835344824856d6f670bdb114
noahtucker <nrt218@lehigh.edu>
2014-08-30 22:33:18 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+// Noah Tucker 
+// CSE2 hw01
+public class WelcomeClass{
+    
+    public static void main(String[] args){
+        
+        System.out.println("   -----------      ");
+        System.out.println("   | WELCOME | ");
+        System.out.println("   ----------- ");
+        System.out.println("   ^  ^  ^  ^  ^  ^ ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println(" |-N--R--T--2--1--8-| ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("   v  v  v  v  v  v ");
+        System.out.println(" I am a huge fan of the New York Mets. "); 
+        System.out.println(" Despite their losing ways, I remain a loyal fan. ");
+    }
+}
