a56269a5d3da7f9accc6f250fca4c30cecd75ded
noahtucker <nrt218@lehigh.edu>
2014-09-08 15:09:45 +0000

    updated Arithmetic.java

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -9,6 +9,8 @@ double taxPercent=0.06; // Sales tax in PA
 int nSocks=3; //Number of pairs of socks
 double sockCost$=2.58; //Cost per pair of socks
 //(‘$’ is part of the variable name)
+System.out.println("I am buying 3 pairs of socks.");
+System.out.println("A pair of socks cost $2.58 per pair.");
 double priceOfSocks=sockCost$*nSocks; //Calculates the total costs of socks before tax
 System.out.println(priceOfSocks+ " is the cost of socks before tax.");
 double taxForSocks= priceOfSocks* taxPercent; // Calculates the sales tax on the socks
@@ -18,6 +20,8 @@ System.out.println(totalCostOfSocks+ " is the total amount the socks cost includ
 
 int nGlasses=6; //Number of drinking glasses
 double glassCost$=2.29; //Cost per glass
+System.out.println("I am buying 6 drinking glasses.");
+System.out.println("Drinking glasses cost $2.29 per glass.");
 double priceOfGlasses = nGlasses*glassCost$; //Calculates the cost of Glasses before tax
 System.out.println(priceOfGlasses+ " is the cost of glasses before tax.");
 double taxForGlasses= priceOfGlasses*taxPercent; //Calculates the sales tax on the glasses
@@ -27,6 +31,8 @@ System.out.println(totalCostOfGlasses+ " is the total amount the glasses cost in
 
 int nEnvelopes=1; //Number of boxes of envelopes
 double envelopeCost$=3.25; //cost per box of envelopes
+System.out.println("I am buying 1 box of envelopes.");
+System.out.println("A box of envelopes cost $3.25 per box.");
 double priceOfEnvelopes= nEnvelopes*envelopeCost$; //Calculates the cost of envelopes before tax
 System.out.println(priceOfEnvelopes+ " is the cost of envelopes before tax.");
 double taxForEnvelopes= taxPercent*priceOfEnvelopes; //Calculates the sales tax on the envelopes
