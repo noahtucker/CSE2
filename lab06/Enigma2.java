9ae17122828b95089dee5ca3132b4ac3c4d6f6eb
noahtucker <nrt218@lehigh.edu>
2014-10-30 23:38:44 +0000

    updated Enigma2.java

--- lab06/Enigma2.java
+++ lab06/Enigma2.java
@@ -1,19 +1,45 @@
 //Noah Tucker 
-//10/10/14
-//CSE2
+//10/31/14
+//CSE2 
+
+//This lab helps with debugging 
+
 
 import java.util.Scanner;
 public class Enigma2{
   public static void main(String [] arg){
     int n=40,k=60;
     String out="";
+try{
     switch(n){
       case 12: 
       case 13: out+=13;
-      case 40: out+=40;
+      case 40: out+=40/0;
+
+     
     }
-    System.out.println(out);
-  }
+        System.out.println(out);
 }
 
-//There is a runtime error for dividing by zero in line 13 (it was removed)
+  catch(ArithmeticException e){				
+	System.out.println("arithmetic error.");
+}
+finally
+{
+
+	return;
+	
+}
+}
+}
+
+/*
+ * Error Report: Explain how you can use the java.lang exception to
+ *     identify the runtime error in the above code
+ * When using the java.lang exception, a try statement, a catch statement and a finally statement are added. 
+ * A try statement is added around the switch statement and the other two are added at the bottom of the main method. 
+ * The arithmetic error is caught this way and return a statement that prints "arithmetic error." 
+ * 
+ * 
+ */
+
